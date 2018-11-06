308214c2cb3603e0d42d734b4bbcfdcadb2b5a78
Stewart ilondanga <stilondanga@gmail.com>
2018-11-06 10:34:29 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,10 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+setTimeout(function(){var rellax = new Rellax('.rellax')},50);
+
+window.addEventListener('scroll', function(){
+  document.querySelector('.img').style.opacity = (1 - window.scrollY / (window.innerHeight * 0.75)) * 0.5;
+  document.querySelector('.img img').style.filter = `blur(${(window.scrollY / (window.innerHeight * 0.75)) * 5}px)`;
+});
